6547f70c01cb66dbfccfc3367adffec07ef289c4
Shlomo_Daniel <shlomodani054@gmail.com>
2024-02-01 20:40:09 +0200

    first commit

---
+++ Lesson 03 - 01.02.2024/script.js
@@ -0,0 +1,9 @@
+function task1() {
+    const answer = document.getElementById("answer1").value;
+
+    if (answer == "yellow") {
+        document.getElementById("output1").innerHTML = "ripe"
+    } else {
+        document.getElementById("output1").innerHTML = "unripe"
+    }
+}
