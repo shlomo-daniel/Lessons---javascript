1dbce23ba589a5f9af3931d51199affc0d2b774f
shlomo-daniel <shlomodani054@gmail.com>
2024-03-11 21:49:15 +0200

    11.03.2024

--- Lesson 12 - 04.03.2024/Stoper/script.js
+++ Lesson 12 - 04.03.2024/Stoper/script.js
@@ -1,46 +1,49 @@
-// 
-// task: crate a stoper that record the current time
-// 
-
-// overview
-// 1- crate digital clock
-// 2- button that stop the time
-// 3- button that record the current time but not stoping the clock
-
-
-
 // digital clock element selector
-const digital = document.querySelector("#digital-clock");
+let digital = document.querySelector("#digital-clock");
+
+let stoper_minutes = 0;
+let stoper_seconds = 0;
+let stoper_milisec = 0;
+let stoper_hours = 0;
 
 
-function clockMotion() {
-    // crate object
-    const theTime = new Date();
-
-    // get the real time
-    const miliseconds = theTime.getMilliseconds();
-    const seconds = theTime.getSeconds();
-    const minutes = theTime.getMinutes();
-    const hours = theTime.getHours();
-
-
-
-    const digitalClock = document.querySelector("#digital-clock")
-
-    if (miliseconds > 850 && miliseconds < 1000) {
-        digitalClock.innerHTML = `${hours} ${minutes} ${seconds} ${miliseconds}`
-    } else {
-        digitalClock.innerHTML = `${hours}:${minutes}:${seconds}:${miliseconds}`
-    }
-    setInterval(clockMotion, 100)
-}
-function stoper() {
-
-    const stoper_seconds = 0;
-    const stoper_minutes = 0;
-    const stoper_milisec = 0;
-
-    stoper_seconds++
-    stoper_minutes++
+function stoperMili() {
     stoper_milisec++
+    if (stoper_milisec >= 1000) {
+        stoper_milisec = 0;
+    }
+    digital.innerHTML = `${stoper_minutes}:${stoper_seconds}:${stoper_milisec}`;
 }
+
+function stoperSec() {
+    stoper_seconds++
+    if (stoper_seconds >= 60) {
+        stoper_seconds = 0;
+    }
+    digital.innerHTML = `${stoper_minutes}:${stoper_seconds}:${stoper_milisec}`;
+}
+function stoperMin() {
+    stoper_minutes++
+    digital.innerHTML = `${stoper_minutes}:${stoper_seconds}:${stoper_milisec}`;
+}
+
+setInterval(stoperMili, 1);
+setInterval(stoperMili, 1);
+setInterval(stoperMili, 1);
+setInterval(stoperMili, 1);
+
+setInterval(stoperSec, 1000);
+setInterval(stoperMin, 60000);
+
+
+let capturePeregraph = document.querySelector(".capture-lap")
+
+function captureByButton() {
+    const capture = digital.innerHTML;
+    capturePeregraph.innerHTML = capture;
+}
+
+// function captureNow(capturePeregraph) {
+//     const capture = digital.innerHTML;
+//     capturePeregraph.innerHTML = capture;
+// }
